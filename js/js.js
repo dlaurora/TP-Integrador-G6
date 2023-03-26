0ddcde1e1f444815d9f7f98210c9aa5c05671525
dlaurora <diegolaurora@gmail.com>
2023-03-26 20:11:09 -0300

    Se agrego, headder con logo, tituos, seccionesy un formulario

---
+++ js/js.js
@@ -0,0 +1,31 @@
+const form = document.getElementById("formulario");
+
+function enviarFormulario() {
+  const nombre = document.getElementById("nombre").value;
+  const apellido = document.getElementById("apellido").value;
+  const telefono = document.getElementById("telefono").value;
+  const email = document.getElementById("email").value;
+  const bicicleta = document.getElementById("bicicleta").value;
+  const costo = document.getElementById("costo").value;
+
+  function limpiarFormulario() {
+    document.getElementById("nombre").value = '';
+    document.getElementById("apellido").value = '';
+    document.getElementById("telefono").value = '';
+    document.getElementById("email").value = '';
+    document.getElementById("bicicleta").value = '';
+    document.getElementById("costo").value = '';
+  }
+
+  if (nombre === "" || apellido === "" || telefono === "" || email === "" || bicicleta === "" || costo === "") {
+    alert("Formulario Incompleto");
+  } else {
+    alert("Pedido enviado");
+    limpiarFormulario();
+  }
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  enviarFormulario();
+});
