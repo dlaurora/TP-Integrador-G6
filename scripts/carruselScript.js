b09eec6f038fd22851dda706fd8b8ecfca17c7c4
Gonzalo <gonzalo.gonzalez@sg.gba.gob.ar>
2023-03-31 10:38:34 -0300

    carrusel sin bootstrap 1.0

--- scripts/carruselScript.js
+++ scripts/carruselScript.js
@@ -1,35 +1,40 @@
-const divCarruselIndicatorsBicis = document.getElementById('carouselExampleCaptions');
-const divCarruselBtnsBicis = document.getElementById('carousel-indicators');
-const divCarouselInnerBicis = document.getElementById('carousel-inner');
-const form = document.getElementById("formulario");
+const carouselImages = document.querySelector("#carousel-images");
+const images = []
+
+let currentIndex = 0;
+
+const showImage = () => {
+    carouselImages.innerHTML = `
+        <img src="${images[currentIndex].imagen}" alt="${images[currentIndex].modelo}">
+        <div class="caption">${images[currentIndex].modelo} - ${images[currentIndex].precio}</div>
+    `;
+}
+
+const nextImage = () => {
+    currentIndex++;
+    if (currentIndex === images.length) {
+        currentIndex = 0;
+    }
+    showImage();
+}
+
+const prevImage = () => {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = images.length - 1;
+    }
+    showImage();
+    }
 
 
-
-    fetch('./database/bicicletas.json')
-  .then(response => response.json())
-  .then(data => {
-    // Iterar sobre los datos y crear los elementos del carrusel
-    data.forEach((element, index) => {
-      const isActive = index === 0 ? 'active' : '';
-      carouselIndicators.innerHTML += `
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="${index}" class="${isActive}" aria-current="true"></button>
-      `;
-      carouselInner.innerHTML += `
-        <div class="carousel-item ${isActive}">
-          <img src="${element.imagen}" class="d-block w-100" alt="${element.titulo}">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>${element.marca}</h5>
-            <p>${element.modelo}</p>
-            <p>${element.precio} USD el dia</p>
-          </div>
-        </div>
-      `;
-    });
-
-    // Inicializar el carrusel de Bootstrap
-    const carousel = new bootstrap.Carousel(document.querySelector('#carouselExampleIndicators'), {
-      interval: 5000, // tiempo de transición entre imágenes en ms
-      pause: 'hover', // pausa la transición cuando el usuario coloca el mouse sobre el carrusel
-    });
-  })
-  .catch(error => console.error(error));
+fetch('./database/bicicletas.json')
+    .then(res => res.json())
+    .then(data => {
+        for (const i of data) {        
+            images.push(i);
+        }
+        showImage();
+    document.querySelector("#prevBtn").addEventListener("click", prevImage);
+    document.querySelector("#nextBtn").addEventListener("click", nextImage);
+    })
+    .catch(error => console.error(error));
